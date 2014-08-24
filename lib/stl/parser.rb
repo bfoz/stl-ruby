cc302774ec9f96a5ee02899538997493f3e17948
Brandon Fosdick <bfoz@bfoz.net>
2014-08-23 20:33:10 -0700

    Added support for reading binary STL files

--- lib/stl/parser.rb
+++ lib/stl/parser.rb
@@ -1,15 +1,25 @@
 require 'geometry'
 
 module STL
+    # http://en.wikipedia.org/wiki/STL_(file_format)
     class Parser
 	# @param io [IO]    the stream to parse
+	# @return [Array]   An array of [Normal, Triangle] pairs
 	def self.parse(io)
-	    self.new.parse(io)
+	    # A binary STL file has an 80 byte header that should never contain
+	    #  the word 'solid'. The first non-whitespace characters of an ASCII
+	    #  STL file should contain 'solid'.
+	    if io.gets(80).include?('solid')
+		self.new.parse_ascii(io.rewind)
+	    else
+		self.new.parse_binary(io)
+	    end
 	end
 
+	# Parse an ASCII STL file
 	# @param io [IO]    the stream to parse
 	# @return [Array]   An array of [Normal, Triangle] pairs
-	def parse(io)
+	def parse_ascii(io)
 	    stack = []
 	    triangles = []
 	    io.each do |line|
@@ -25,6 +35,22 @@ module STL
 		end
 	    end
 	    triangles
+	end
+
+	# Parse a binary STL file, assuming that the header has already been read
+	# @param io [IO]    the stream to parse
+	# @return [Array]   An array of [Normal, Triangle] pairs
+	def parse_binary(io)
+	    count = io.read(4).unpack('V').first
+
+	    faces = []
+	    while not io.eof?
+		normal, *vertices = io.read(50).unpack('F3F3F3F3x2').each_slice(3).to_a
+		faces.push [normal, Geometry::Triangle.new(*vertices)]
+	    end
+	    raise StandardError, "Unexpected end of file after #{faces.length} triangles" if faces.length != count
+
+	    faces
 	end
     end
 end
